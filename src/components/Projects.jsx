48d9e5ec84c170b9ec50e47a0fc9e5128a621b58
unknown <zbanjar9@gmail.com>
2025-08-28 10:21:34 +0700

    add portfolio project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,16 +2,9 @@ import React from 'react'
 import pokedex from '../assets/pokedex.mp4'
 import coffee from '../assets/coffee-tracker.mp4'
 import student from '../assets/student-management.mp4'
+import portfolio from '../assets/portfolio.mp4'
 
 const projects = [
-    {
-        name: 'Pokedex',
-        desc: 'A web app that serves as a digital Pokédex, built using the PokéAPI It allows users to search for Pokémon and view detailed information such as type, abilities, stats, and sprites. The project highlights my skills in working with RESTful APIs.',
-        web: 'https://pokedex-zet.netlify.app',
-        git: 'https://github.com/zayyanza/pokedex',
-        vid: pokedex,
-        tools: ['React', 'Tailwind', 'Typescript', 'Shadcn/ui', 'REST API']
-    },
     {
         name: 'Coffee Intake Tracker',
         desc: 'A web app to track coffee consumption. Users can log different coffee types along with price and time, and the app calculates stats like active caffeine level, daily intake, cost, and history. Data is stored with Firebase.',
@@ -21,18 +14,35 @@ const projects = [
         tools: ['React', 'Tailwind', 'Typescript', 'Shadcn/ui', 'Firebase']
     },
     {
+        name: "Zayyan's Portfolio",
+        desc: 'A modern and responsive personal website built to showcase my skills, projects, and experience as a developer.',
+        git: 'https://github.com/zayyanza/portfolio',
+        web: 'https://zayyanza.netlify.app',
+        vid: portfolio,
+        tools: ['React', 'Tailwind']
+    },
+    {
         name: 'Student Management',
-        desc: 'A web app to track coffee consumption. Users can log different coffee types along with price and time, and the app calculates stats like active caffeine level, daily intake, cost, and history. Data is stored with Firebase for persistence and easy access.',
-        git: 'https://github.com/zayyanza/coffee-intake-tracker',
+        desc: 'A CRUD-based web app for managing student records. It lets users add, edit, search, and delete students with a clean interface, search functionality, and pagination.',
+        git: 'https://github.com/zayyanza/student-management-laravel',
         vid: student,
         tools: ['Laravel', 'MySQL']
-    }
+    },
+    {
+        name: 'Pokedex',
+        desc: 'A web app that serves as a digital Pokédex, built using the PokéAPI It allows users to search for Pokémon and view detailed information such as type, abilities, stats, and sprites. The project highlights my skills in working with RESTful APIs.',
+        web: 'https://pokedex-zet.netlify.app',
+        git: 'https://github.com/zayyanza/pokedex',
+        vid: pokedex,
+        tools: ['React', 'Tailwind', 'Typescript', 'Shadcn/ui', 'REST API']
+    },
 ]
 
 function ProjectCard(props) {
     return (
         <div className='flex flex-col gap-5 p-4 bg-bg-variant rounded-2xl justify-between'>
-            <div className='rounded-2xl'>
+            <div className='flex flex-col gap-5'>
+                <div className='rounded-2xl'>
                 <video
                     src={props.vid}
                     autoPlay
@@ -50,6 +60,7 @@ function ProjectCard(props) {
                 ))}
             </div>
             <p className='text-light text-sm'>{props.desc}</p>
+            </div>
             <div className='flex gap-4'>
                 {props.web && <a href={props.web} className='btn ' target='_blank'>Website</a>}
                 {props.git && <a href={props.git} className='btn' target='_blank'>Github</a>}
